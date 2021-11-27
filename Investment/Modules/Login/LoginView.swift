4f6862bfb0d3bd901c35da85aad983f6ac81c4b7
Daniel <krivelevdaniil@gmail.com>
2021-11-27 19:31:26 +0700

    mockup done for register

--- Investment/Modules/Login/LoginView.swift
+++ Investment/Modules/Login/LoginView.swift
@@ -7,7 +7,7 @@ import SwiftUI
 
 struct LoginView: UIViewControllerRepresentable {
   func makeUIViewController(context: Context) -> UIViewController {
-    return LoginViewController()
+    return LoginViewController(nibName: "LoginViewController", bundle: nil)
   }
   func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
     
--- Investment/Modules/Login/LoginViewController.swift
+++ Investment/Modules/Login/LoginViewController.swift
@@ -1,23 +1,26 @@
 //
-//  LoginViewController.swift
+//  LoginViewControllerXIB.swift
 //  Investment
+//
+//  Created by Daniil on 27.11.2021.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
-  private let viewModel: LoginViewModel = LoginViewModel()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .blue
-  }
-  
-  init() {
-    super.init(nibName: nil, bundle: nil)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    
+    private let viewModel: LoginViewModel = LoginViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "StartScreenBackground")
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.backgroundColor = UIColor(named: "StartScreenBackground")
+    }
+    
+    
+    
 }
